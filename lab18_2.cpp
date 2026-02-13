27e2b3f1122aa3fb03908c0ca534b9812ab46fd5
Thammarat Pulthawonsin <tmrptws@Gee-2.local>
2026-02-13 13:21:43 +0700

    completed lab18_2

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,12 +1,26 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
 
+double overlap(Rect* R1, Rect* R2){
+  
+    double left = max(R1->x, R2->x);
+    double right = min(R1->x + R1->w, R2->x + R2->w);
+    double width = right - left;
 
+    double top = min(R1->y, R2->y);
+    double bottom = max(R1->y - R1->h, R2->y - R2->h);
+    double height = top - bottom;
 
+    if(width <= 0 || height <= 0){
+        return 0;
+    }
+
+    return width * height;
 }
+
